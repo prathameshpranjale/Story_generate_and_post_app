b8e7462c84e406e4f41ce592dafdbfe25ed15581
Prathamesh Pranjale <prathameshpranjale777@gmail.com>
2024-09-08 13:58:08 +0530

    logout working

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -132,9 +132,34 @@ const loginUser = asyncHandler(async (req, res) => {
     }
 });
 
+const logoutUser = asyncHandler(async (req, res) => {
+    // remove database refresh token and cookies that is storing login
+    // refreshtoken and acess token 
+    User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: { refreshToken: undefined }
+        },
+        {
+            new: true
+        }
+    )
 
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    console.log("logout successfuly");
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "User Logged Out Successfully"))
+})
 
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 };
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
-import {registerUser,loginUser} from '../controllers/user.controller.js'
+import {registerUser,loginUser, logoutUser} from '../controllers/user.controller.js'
 import { upload } from '../middlewares/multer.middleware.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
 
 
 const router = Router();
@@ -19,6 +20,7 @@ router.route("/register").post(([
 // router.post('/register', upload.single('profilePicture'), registerUser);
 
 router.route("/login").post(loginUser)
+router.route("/logout").post(verifyJWT,logoutUser)
 
 
 
